e9246b11671a746f0df24c4b8db2ce6c09c1d21e
Alex <alexzoetermeer@gmail.com>
2019-01-21 21:20:24 +0100

    Separated out the character info as view

---
+++ resources/views/character/info.blade.php
@@ -0,0 +1,24 @@
+<table class="table table-sm table-dark">
+    <tbody>
+        <tr>
+            <td>
+                <span>
+                    <span aria-hidden="true" class="li_user"></span> 
+                    <a href="#" class="">{{ Auth::user()->character->name }}</a>
+                    <br>
+                    <span aria-hidden="true" class="li_heart"></span> {{ Auth::user()->character->life }}%
+                    <span aria-hidden="true" class="li_data pl-2"></span> {{ Auth::user()->character->rank() }}
+                </span>
+                <span class="float-right"><span aria-hidden="true" class="li_stack"></span> Messages (0)</span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <span aria-hidden="true" class="li_banknote"></span> &euro; {{ number_format(Auth::user()->character->money, 0, '.', '.') }},-
+                <span aria-hidden="true" class="li_truck pl-2"></span> {{ Auth::user()->character->contraband }}kg
+                <span aria-hidden="true" class="li_location pl-2"></span> {{ str_replace('Of', 'of', ucwords(Auth::user()->character->country)) }}
+                <span aria-hidden="true" class="li_world pl-2"></span> {{ ucfirst(Auth::user()->character->transport) }}
+            </td>
+        </tr>
+    </tbody>
+</table>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,39 +4,12 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10 col-lg-8">
-            <table class="table table-sm table-dark">
-                <tbody>
-                    @if (session('status'))
-                        <tr>
-                            <td>
-                                <div class="alert alert-success" role="alert">
-                                    {{ session('status') }}
-                                </div>
-                            </td>
-                        </tr>
-                    @endif
-                    <tr>
-                        <td>
-                            <span>
-                                <span aria-hidden="true" class="li_user"></span> {{ Auth::user()->character->name }}
-                                <a href="#" class="float-right">(edit profile)</a>
-                                <br>
-                                <span aria-hidden="true" class="li_heart"></span> {{ Auth::user()->character->life }}%
-                                <span aria-hidden="true" class="li_data pl-2"></span> {{ Auth::user()->character->experience }}
-                            </span>
-                            <span class="float-right"><span aria-hidden="true" class="li_stack"></span> Messages (0)</span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <span aria-hidden="true" class="li_banknote"></span> &euro; {{ Auth::user()->character->money }},-
-                            <span aria-hidden="true" class="li_truck pl-2"></span> {{ Auth::user()->character->contraband }}kg
-                            <span aria-hidden="true" class="li_location pl-2"></span> {{ Auth::user()->character->country }}
-                            <span aria-hidden="true" class="li_world pl-2"></span> {{ Auth::user()->character->transport }}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">asd
+                    {{ session('status') }}
+                </div>
+            @endif
+            @include('character.info')
         </div>
     </div>
     <div class="row justify-content-center">
