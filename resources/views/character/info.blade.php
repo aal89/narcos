2662a9c34b63130baae13154d3f5a7937a0f96bb
Alex <alexzoetermeer@gmail.com>
2019-01-21 21:31:45 +0100

    Added in a link for messages, added a development update

--- resources/views/character/info.blade.php
+++ resources/views/character/info.blade.php
@@ -9,13 +9,16 @@
                     <span aria-hidden="true" class="li_heart"></span> {{ Auth::user()->character->life }}%
                     <span aria-hidden="true" class="li_data pl-2"></span> {{ Auth::user()->character->rank() }}
                 </span>
-                <span class="float-right"><span aria-hidden="true" class="li_stack"></span> Messages (0)</span>
+                <span class="float-right">
+                    <span aria-hidden="true" class="li_stack"></span>
+                    <a href="#" class="">Messages (0)</a>
+                </span>
             </td>
         </tr>
         <tr>
             <td>
                 <span aria-hidden="true" class="li_banknote"></span> &euro; {{ number_format(Auth::user()->character->money, 0, '.', '.') }},-
-                <span aria-hidden="true" class="li_truck pl-2"></span> {{ Auth::user()->character->contraband }}kg
+                <span aria-hidden="true" class="li_truck pl-2"></span> {{ number_format(Auth::user()->character->contraband, 0, '.', '.') }}kg
                 <span aria-hidden="true" class="li_location pl-2"></span> {{ str_replace('Of', 'of', ucwords(Auth::user()->character->country)) }}
                 <span aria-hidden="true" class="li_world pl-2"></span> {{ ucfirst(Auth::user()->character->transport) }}
             </td>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,10 +25,28 @@
                         <td>
                             <div class="container">
                                 <div class="row">
+                                    <h5 class="">Update #1</h5>
+                                </div>
+                                <div class="row">
+                                    - Created character creation and death controllers, views and migrations.<br>
+                                    - Created middleware to respond accordingly to character state (see above line).<br>
+                                    - Cleanup code a lot.<br>
+                                    - Added comments throughout code base.<br>
+                                </div>
+                                <div class="row">
+                                    <span class="small mt-2">21-01-2019 21:23 by aal</span>
+                                </div>
+                                <p>&nbsp;</p>
+                                <div class="row">
                                     <h5 class="">Development</h5>
                                 </div>
                                 <div class="row">
-                                    Groundwork done.
+                                    Groundwork done.<br>
+                                    - Scaffoulded out a authentication system.<br>
+                                    - Created a css to reflect a mafia/narcos style game.<br>
+                                    - Created logo.<br>
+                                    - Setup environmental variables, so that emailing and verifying works etc.<br>
+                                    - Setup deployment pipeline.<br>
                                 </div>
                                 <div class="row">
                                     <span class="small mt-2">13-01-2019 21:43 by aal</span>
