059926242090ddfa235f09e4c8b2833d26faa0fc
Alex <alexzoetermeer@gmail.com>
2019-01-22 20:43:52 +0100

    Added collapsable menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,14 +1,42 @@
 <table class="table table-sm table-dark">
     <thead>
         <tr>
-            <th><span aria-hidden="true" class="li_params"></span> {{ __('Menu') }}</th>
+            <th>
+                <span aria-hidden="true" class="li_params"></span> 
+                <a data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    {{ __('Menu') }}
+                </a>
+            </th>
         </tr>
     </thead>
     <tbody>
+        
+            <tr>
+                <td>
+                <div class="collapse" id="collapseExample">
+                    Menu</div>
+                </td>
+            </tr>
+        
+    </tbody>
+    <thead>
         <tr>
-            <td>
-                Menu
-            </td>
+            <th>
+                <span aria-hidden="true" class="li_params"></span> 
+                <a data-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample2">
+                    {{ __('Menu') }}
+                </a>
+            </th>
         </tr>
+    </thead>
+    <tbody>
+        
+            <tr>
+                <td>
+                <div class="collapse" id="collapseExample2">
+                    Menu</div>
+                </td>
+            </tr>
+        
     </tbody>
 </table>
