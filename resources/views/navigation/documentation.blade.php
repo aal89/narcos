43e07c2632c3fa8fab01b6d11df16c1c954ea3fa
Alex <alexzoetermeer@gmail.com>
2019-02-02 17:54:17 +0100

    Added documentation for the profile description

--- resources/views/navigation/documentation.blade.php
+++ resources/views/navigation/documentation.blade.php
@@ -13,10 +13,47 @@
                 <p>All calculations, additions and substractions regarding your character and the game are described in more detail on this page. However you cannot derive any rights from this page. They are merely meant as informative. You could use it to determine a strategy.</p>
             </div>
             <div class="row">
-                <h5 class="text-secondary">Banking</h5>
+                <h5 class="text-secondary" id="profile">Profile</h5>
             </div>
             <div class="row">
-                <p>TODO</p>
+                <p>
+                    Editing your profile description is easy. Just open your own profile and edit it at the bottom of the page. You can also use <b>NarcoScript</b> to make your profile stand out.
+                </p>
+                <p>
+                    In this chapter you will learn about all the <b>NarcoScript</b> tags, how to use them and what effect they have. At the end of this section there are some examples that you can use.
+                </p>
+                <p><i>The parts in red are to be filled in by you. All other (white) parts are called the NarcoScript tags and can't be changed. They should be used as noted.</i></p>
+                <table class="table table-sm table-dark">
+                    <thead>
+                        <th colspan="2">NarcoScript tags</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="w-50">:b:<font color=red>Bold text</font>:/b:</td>
+                            <td><b>Bold text</b></td>
+                        </tr>
+                        <tr>
+                            <td>:img:<font color=red>https://via.placeholder.com/30</font>:/img:</td>
+                            <td><img src="https://via.placeholder.com/30" /></td>
+                        </tr>
+                        <tr>
+                            <td>:img-center:<font color=red>https://via.placeholder.com/30</font>:/img-center:</td>
+                            <td><img class="mx-auto d-block" src="https://via.placeholder.com/30" /></td>
+                        </tr>
+                        <tr>
+                            <td>:center:<font color=red>Some text</font>:/center:</td>
+                            <td><p class="text-center mb-0">Some text</p></td>
+                        </tr>
+                        <tr>
+                            <td>:br:</td>
+                            <td><br><i>*An empty line*</i></td>
+                        </tr>
+                        <tr>
+                            <td>:link:<font color=red>https://www.google.com</font>:-link:<font color=red>Google</font>:/link:</td>
+                            <td><a href="https://www.google.com">Google</a></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <div class="row">
                 <h5 class="text-secondary">etc</h5>
