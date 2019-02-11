f746cb2633ad51d069f78b9d21d5d8ee5f764d72
Alex <alexzoetermeer@gmail.com>
2019-02-11 20:39:42 +0100

    Deposit and withdrawal functions implemented

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Character;
 
 class BankController extends Controller
 {
@@ -28,12 +30,69 @@ class BankController extends Controller
     }
 
     /**
-     * Deposits money in the bank.
+     * Deposits or withdraw money from the bank.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function postIndex()
+    public function postIndex(Request $request)
     {
-        return view('menu.banking.banking');
+        $this->validate($request, [
+            'amount' => 'required|integer',
+        ]);
+
+        $char = Auth::user()->character;
+
+        switch($request->action)
+        {
+            case 'deposit':
+                try {
+                    $this->deposit(abs(intval($request->amount)), $char);
+                    return redirect()->back();
+                } catch(\Exception $e) {
+                    return redirect()->back()->withErrors(['amount' => $e->getMessage()]);
+                }
+            case 'withdraw':
+                try {
+                    $this->withdraw(abs(intval($request->amount)), $char);
+                    return redirect()->back();
+                } catch(\Exception $e) {
+                    return redirect()->back()->withErrors(['amount' => $e->getMessage()]);
+                }
+            default: return redirect()->back()->withErrors(['amount' => 'Hmm, you might have to try that again.']);
+        }
+    }
+
+    /**
+     * Deposits a given amount to the given characters bank account.
+     * Throws errors if attempt to deposit failed, for example when the user
+     * does not have sufficient funds.
+     */
+    private function deposit(int $amount, \App\Character $char)
+    {
+        if ($char->money >= $amount) {
+            $char->money -= $amount;
+            $char->bank->money += $amount;
+            $char->save();
+            $char->bank->save();
+            return;
+        }
+        throw new \Exception('Insufficient funds on hand.');
+    }
+
+    /**
+     * Withdraws a given amount from the given characters bank account.
+     * Throws errors if attempt to deposit failed, for example when the user
+     * does not have sufficient funds.
+     */
+    private function withdraw(int $amount, \App\Character $char)
+    {
+        if ($char->bank->money >= $amount) {
+            $char->money += $amount;
+            $char->bank->money -= $amount;
+            $char->save();
+            $char->bank->save();
+            return;
+        }
+        throw new \Exception('Insufficient funds on bank account.');
     }
 }
