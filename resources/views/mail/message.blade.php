e00addd97ccfed1252d1710a9abfb3b977953baf
alex <alexzoetermeer@gmail.com>
2019-02-20 10:13:19 +0100

    Updated email template for InGameMessage

--- resources/views/mail/message.blade.php
+++ resources/views/mail/message.blade.php
@@ -1,33 +1,14 @@
-<table class="table table-sm table-dark">
-    <thead>
-        <tr>
-            <th colspan="2"><a href="{{ env('APP_URL') }}/profile/{{ $msg->sender->name }}">{{ $msg->sender->name }}</a></th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>
-                <span><strong>Subject</strong></span>
-            </td>
-            <td>
-                <span class="not-bold text-wrap">{{ $msg->subject }}</span>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <span><strong>Message</strong></span>
-            </td>
-            <td>
-                <span class="not-bold text-wrap">{{ $msg->message }}</span>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2">
-                <small>{{ $msg->created_at }}</small>
-            </td>
-        </tr>
-    </tbody>
-</table>
-<p>
-    <a href="{{ env('APP_URL') }}/messages/inbox">Open messages</a>
-</p>
+@component('mail::message')
+# You received an in-game message!
+
+**{{ $msg->sender->name }}** sent: 
+
+{{ $msg->message }}
+
+@component('mail::button', ['url' => '{{ env('APP_URL') }}/messages/inbox'])
+Open Messages
+@endcomponent
+
+Regards,<br>
+{{ config('app.name') }}
+@endcomponent
