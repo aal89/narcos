8f4ef7225177927e5baf5c2a9536f096de564e8e
Alex <alexzoetermeer@gmail.com>
2019-02-27 21:35:17 +0100

    Added online player view

---
+++ resources/views/menu/online-players/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.basic')
+
+@section('page')
+<h3 class="page-title"><span aria-hidden="true" class="li_user"></span> {{ __('Online players') }}</h3>
+<p>
+    There are {{ count($onlineCharacters) }} characters online right now.
+</p>
+@foreach ($onlineCharacters as $character)
+    <a href="/profile/{{ $character }}">{{ $character }}</a> | 
+@endforeach
+@endsection
