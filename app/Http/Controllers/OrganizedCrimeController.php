a59909d45598b7b24e4d5f10f3af0177775b1acd
Alex <alexzoetermeer@gmail.com>
2019-03-14 21:46:49 +0100

    The inviter now gets notified when an invitation is accepted

--- app/Http/Controllers/OrganizedCrimeController.php
+++ app/Http/Controllers/OrganizedCrimeController.php
@@ -66,6 +66,7 @@ class OrganizedCrimeController extends Controller
                 if ($inviterParty->$position === null) {
                     $inviterParty->{$position.'_id'} = $char->id;
                     $inviterParty->save();
+                    messageComposer($char->id, $inviter->id, 'I\'m your '.$position.'!', 'Let\'s do this!');
                     return redirect('/organized-crime')->with(['status' => 'You have joined as a '.$position.'!' ]);
                 }
                 return redirect('/organized-crime')->withErrors([ 'general' => 'The spot '.$position.' has already been taken by somebody else!' ]);
@@ -75,6 +76,7 @@ class OrganizedCrimeController extends Controller
                 $party->robber_id = $inviter->id;
                 $party->{$position.'_id'} = $char->id;
                 $party->save();
+                messageComposer($char->id, $inviter->id, 'I\'m your '.$position.'!', 'Let\'s do this!');
                 return redirect('/organized-crime')->with([ 'status' => 'You have joined as a '.$position.'!' ]);
             }
         }
