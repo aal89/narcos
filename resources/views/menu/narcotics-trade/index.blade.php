aab19b0bd9f83796a48d6f5101e0543739588758
Alex <alexzoetermeer@gmail.com>
2019-03-18 20:38:53 +0100

    Added basic view for narcotics trade

--- resources/views/menu/narcotics-trade/index.blade.php
+++ resources/views/menu/narcotics-trade/index.blade.php
@@ -2,5 +2,49 @@
 
 @section('page')
 <h3 class="page-title"><span aria-hidden="true" class="li_truck"></span> {{ __('Narcotics trade') }}</h3>
-{{ var_dump($prices) }}
+<p>
+    Smuggling narcotics into other countries can be a lucrative business. Buy low and sell high, be sure to ask around for the current drugroute
+    (changes every 24hrs). Your rank determines how much kilograms you can carry.
+</p>
+<p>
+    <b>You can carry:</b> 2kg
+</p>
+<table class="table table-sm table-dark">
+    <thead>
+        <tr>
+            <th colspan="4">{{ __('Narcotics') }}</th>
+        </tr>
+        <tr class="bg-dark">
+            <th>{{ __('Name') }}</th>
+            <th>{{ __('Price per kg') }}</th>
+            <th>{{ __('Trade') }}</th>
+            <th>{{ __('Actions') }}</th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach ($prices as $narcotic => $price)
+        <tr>
+            <td>
+                <b>{{ ucfirst($narcotic) }}</b>
+            </td>
+            <td>
+                &euro;{{ $price }},-
+            </td>
+            <td>
+                <div class="form-group input-group-sm mb-0">
+                    <input id="buysell" type="number" class="form-control{{ $errors->has('buysell') ? ' is-invalid' : '' }} w-50" name="buysell" value="{{ old('buysell') }}" placeholder="Amount (e.g. 1)" required autofocus>
+                    @if ($errors->has('buysell'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('buysell') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </td>
+            <td class="cell-fit">
+                <button class="btn btn-link">Buy</button><button class="btn btn-link">Sell</button>
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
 @endsection
