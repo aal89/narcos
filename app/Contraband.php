c75801ded9b1429dc3609a3a6736258a8a5fb858
Alex <alexzoetermeer@gmail.com>
2019-03-23 21:32:53 +0100

    Made a narcotic handler and separate logic for buying and selling

--- app/Contraband.php
+++ app/Contraband.php
@@ -11,11 +11,18 @@ class Contraband extends Model
     // can carry a power of x for your rank of kgs (hitman == rank 3 == 3^2 == 9 kgs)
     private $carryPerRank = 2;
 
+    /**
+     * Decides if this character can carry any additional units of narcotics. Does not account for the
+     * amount of money this character is having on-hand.
+     */
     public function canCarryAdditionalKgs(int $kgs)
     {
         return $this->weed + $this->lsd + $this->speed + $this->cocaine + $kgs <= $this->carryCapacity();
     }
 
+    /**
+     * Returns an integer indicating the amount of units narcotics this player can carry.
+     */
     public function carryCapacity()
     {
         return pow($this->character->rankLevel(), $this->carryPerRank);
--- app/Http/Controllers/NarcoticsController.php
+++ app/Http/Controllers/NarcoticsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Auth;
+use App\Character;
 
 class NarcoticsController extends Controller
 {
@@ -44,30 +45,64 @@ class NarcoticsController extends Controller
         return view('menu.narcotics-trade.index')->with('prices', $pricesForCountryOfChar)->with('carryCapacity', $carryCapacity);
     }
 
+    /**
+     * Request handler for a trade request of a specific narcotic.
+     */
     public function postTrade(Request $request, string $narcotic)
     {
+        $char = Auth::user()->character;
+        $isBuying = $request->action === 'buy' ? true : false;
+        $pricesForCountryOfChar = $this->getCurrentPrices()[$char->country];
         switch($narcotic) {
-            case 'weed': return $this->buyWeed($request);
-            case 'lsd': return $this->buyLsd($request);
-            case 'speed': return $this->buySpeed($request);
-            case 'cocaine': return $this->buyCocaine($request);
-            default: return $this->buyWeed($request);
+            case 'weed':
+                $this->validate($request, ['weed' => 'required|integer|min:1']);
+                if ($isBuying) {
+                    return $this->buyWeed($char, $pricesForCountryOfChar['weed'], $request->weed);
+                } else {
+                    return $this->sellWeed($char, $pricesForCountryOfChar['weed'], $request->weed);
+                }
+            case 'lsd': return $this->buyLsd($request, $char);
+            case 'speed': return $this->buySpeed($request, $char);
+            case 'cocaine': return $this->buyCocaine($request, $char);
+            default: return redirect()->back()->withErrors(['general', 'Hmm, you might want to try that again!']);
         }
     }
 
-    public function buyWeed(Request $request)
+    public function buyWeed(Character $char, int $pricePerKg, int $kgs)
     {
-        $this->validate($request, [
-            'weed' => 'required|integer|min:1'
-        ]);
-        $isBuying = $request->action === 'buy' ? true : false;
+        if (!$char->contraband->canCarryAdditionalKgs($kgs)) {
+            return redirect()->back()->withErrors(['general' => 'You can\'t carry anymore kgs of narcotics.']);
+        }
 
-        var_dump($request);
+        if (!$char->money < $kgs * $pricePerKg) {
+            return redirect()->back()->withErrors(['general' => 'Insufficient funds.']);
+        }
 
-        return null;
+        // by this point we know we can carry it and have sufficient funds for it, do the trade
+        $char->money -= $kgs * $pricePerKg;
+        $char->contraband->weed += $kgs;
+        $char->save();
+        $char->contraband->save();
+
+        return redirect()->back()->with(['status' => 'You just bought '.$kgs.'kgs of weed for €'.$kgs * $pricePerKg.',-.']);
     }
 
-    public function buyLsd(Request $request)
+    public function sellWeed(Character $char, int $pricePerKg, int $kgs)
+    {
+        if($char->contraband->weed < $kgs) {
+            return redirect()->back()->withErrors(['general' => 'You don\'t carry as much.']);
+        }
+
+        // by this point we know we actually have the narcotics on-hand, so do the trade
+        $char->money += $kgs * $pricePerKg;
+        $char->contraband->weed -= $kgs;
+        $char->save();
+        $char->contraband->save();
+
+        return redirect()->back()->with(['status' => 'You just sold '.$kgs.'kgs of weed for €'.$kgs * $pricePerKg.',-.']);
+    }
+
+    public function buyLsd(Request $request, Character $char)
     {
         $this->validate($request, [
             'lsd' => 'required|integer|min:1'
@@ -79,7 +114,7 @@ class NarcoticsController extends Controller
         return null;
     }
 
-    public function buySpeed(Request $request)
+    public function buySpeed(Request $request, Character $char)
     {
         $this->validate($request, [
             'speed' => 'required|integer|min:1'
@@ -91,7 +126,7 @@ class NarcoticsController extends Controller
         return null;
     }
 
-    public function buyCocaine(Request $request)
+    public function buyCocaine(Request $request, Character $char)
     {
         $this->validate($request, [
             'cocaine' => 'required|integer|min:1'
