6d11606b37b61194266f987d220e661d8cf0d8ff
Alex <alexzoetermeer@gmail.com>
2019-04-01 21:27:34 +0200

    Fixed html rendering in trusted messages

--- resources/views/mail/message.blade.php
+++ resources/views/mail/message.blade.php
@@ -1,7 +1,11 @@
 @component('mail::message')
 # **{{ $msg->sender->name }}** sent: 
 
+@if ($msg->trusted)
+{!! $msg->message !!}
+@else
 {{ $msg->message }}
+@endif
 
 @component('mail::button', ['url' => env('APP_URL').'/messages/inbox'])
 Open Messages
