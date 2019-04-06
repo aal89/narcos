eec7c7d002d87115e0293f6fd2a6af9915b62397
Alex <alexzoetermeer@gmail.com>
2019-04-06 16:03:32 +0200

    Finished roulette view

--- resources/views/menu/roulette/index.blade.php
+++ resources/views/menu/roulette/index.blade.php
@@ -59,6 +59,11 @@
                     <input type="number" class="form-control form-control-sm text-center" min="1" placeholder="19 to 36">
                 </div>
             </div>
+            <div class="row">
+                <div class="col text-center">
+                    <button class="btn btn-secondary" type="submit">Wager</button>
+                </div>
+            </div>
         </div>
         <div class="col-7">
             <div class="form-group mb-2 row">
@@ -67,153 +72,148 @@
                 </div>
             </div>
             <div class="form-group mb-1 row">
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pr-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="1">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 p-0">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="2">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pl-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="3">
                 </div>
             </div>
             <div class="form-group mb-1 row">
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pr-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="4">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 p-0">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="5">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pl-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="6">
                 </div>
             </div>
             <div class="form-group mb-1 row">
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pr-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="7">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 p-0">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="8">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pl-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="9">
                 </div>
             </div>
             <div class="form-group mb-2 row">
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pr-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="10">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 p-0">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="11">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pl-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="12">
                 </div>
             </div>
             <div class="form-group mb-1 row">
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pr-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="13">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 p-0">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="14">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pl-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="15">
                 </div>
             </div>
             <div class="form-group mb-1 row">
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pr-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="16">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 p-0">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="17">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pl-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="18">
                 </div>
             </div>
             <div class="form-group mb-1 row">
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pr-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="19">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 p-0">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="20">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pl-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="21">
                 </div>
             </div>
             <div class="form-group mb-2 row">
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pr-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="22">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 p-0">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="23">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pl-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="24">
                 </div>
             </div>
             <div class="form-group mb-1 row">
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pr-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="25">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 p-0">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="26">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pl-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="27">
                 </div>
             </div>
             <div class="form-group mb-1 row">
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pr-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="28">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 p-0">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="29">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pl-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="30">
                 </div>
             </div>
             <div class="form-group mb-1 row">
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pr-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="31">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 p-0">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="32">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pl-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="33">
                 </div>
             </div>
             <div class="form-group mb-2 row">
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pr-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="34">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 p-0">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="35">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pl-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="36">
                 </div>
             </div>
             <div class="form-group mb-2 row">
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pr-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="2 to 1">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 p-0">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="2 to 1">
                 </div>
-                <div class="col-4 col-md-2 col-lg-2">
+                <div class="col-4 col-md-2 col-lg-2 pl-1">
                     <input type="number" class="form-control form-control-sm text-center" placeholder="2 to 1">
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col text-center">
-            <button class="btn btn-secondary" type="submit">Wager</button>
         </div>
     </div>
 </form>
