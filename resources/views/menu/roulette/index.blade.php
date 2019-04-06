27582d813e440c62b1d82e288139dd850993eec3
Alex <alexzoetermeer@gmail.com>
2019-04-07 00:42:09 +0200

    Added identifiers to all fields

--- resources/views/menu/roulette/index.blade.php
+++ resources/views/menu/roulette/index.blade.php
@@ -16,47 +16,47 @@
         <div class="col-5 col-md-2 col-lg-2 my-auto">
             <div class="form-group row mb-1">
                 <div class="col">
-                    <input type="number" class="form-control form-control-sm text-center" min="1" placeholder="1st 12">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n1st12') ? ' is-invalid' : '' }}" name="n1st12" placeholder="1st 12">
                 </div>
             </div>
             <div class="form-group row mb-1">
                 <div class="col">
-                    <input type="number" class="form-control form-control-sm text-center" min="1" placeholder="1 to 18">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n1to18') ? ' is-invalid' : '' }}" name="n1to18" placeholder="1 to 18">
                 </div>
             </div>
             <div class="form-group row mb-2">
                 <div class="col">
-                    <input type="number" class="form-control form-control-sm text-center" min="1" placeholder="Even">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('neven') ? ' is-invalid' : '' }}" name="neven" placeholder="Even">
                 </div>
             </div>
             <div class="form-group row mb-1">
                 <div class="col">
-                    <input type="number" class="form-control form-control-sm text-center" min="1" placeholder="2nd 12">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n2nd12') ? ' is-invalid' : '' }}" name="n2nd12" placeholder="2nd 12">
                 </div>
             </div>
             <div class="form-group row mb-1">
                 <div class="col">
-                    <input type="number" class="form-control form-control-sm text-center roulette-red" min="1" placeholder="Red">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('nred') ? ' is-invalid' : '' }} roulette-red" name="nred" placeholder="Red">
                 </div>
             </div>
             <div class="form-group row mb-2">
                 <div class="col">
-                    <input type="number" class="form-control form-control-sm text-center roulette-black" min="1" placeholder="Black">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('nblack') ? ' is-invalid' : '' }} roulette-black" name="nblack" placeholder="Black">
                 </div>
             </div>
             <div class="form-group row mb-1">
                 <div class="col">
-                    <input type="number" class="form-control form-control-sm text-center" min="1" placeholder="3rd 12">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n3rd12') ? ' is-invalid' : '' }}" name="n3rd12" placeholder="3rd 12">
                 </div>
             </div>
             <div class="form-group row mb-1">
                 <div class="col">
-                    <input type="number" class="form-control form-control-sm text-center" min="1" placeholder="Odd">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('nodd') ? ' is-invalid' : '' }}" name="nodd" placeholder="Odd">
                 </div>
             </div>
             <div class="form-group row mb-1">
                 <div class="col">
-                    <input type="number" class="form-control form-control-sm text-center" min="1" placeholder="19 to 36">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n19to36') ? ' is-invalid' : '' }}" name="n19to36" placeholder="19 to 36">
                 </div>
             </div>
             <div class="row">
@@ -68,150 +68,150 @@
         <div class="col-7">
             <div class="form-group mb-2 row">
                 <div class="col-12 col-md-6 col-lg-6">
-                    <input type="number" class="form-control form-control-sm text-center" min="1" placeholder="0">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n0') ? ' is-invalid' : '' }}" name="n0" placeholder="0">
                 </div>
             </div>
             <div class="form-group mb-1 row">
                 <div class="col-4 col-md-2 col-lg-2 pr-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="1">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n1') ? ' is-invalid' : '' }}" name="n1" placeholder="1">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 p-0">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="2">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n2') ? ' is-invalid' : '' }}" name="n2" placeholder="2">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 pl-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="3">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n3') ? ' is-invalid' : '' }}" name="n3" placeholder="3">
                 </div>
             </div>
             <div class="form-group mb-1 row">
                 <div class="col-4 col-md-2 col-lg-2 pr-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="4">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n4') ? ' is-invalid' : '' }}" name="n4" placeholder="4">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 p-0">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="5">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n5') ? ' is-invalid' : '' }}" name="n5" placeholder="5">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 pl-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="6">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n6') ? ' is-invalid' : '' }}" name="n6" placeholder="6">
                 </div>
             </div>
             <div class="form-group mb-1 row">
                 <div class="col-4 col-md-2 col-lg-2 pr-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="7">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n7') ? ' is-invalid' : '' }}" name="n7" placeholder="7">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 p-0">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="8">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n8') ? ' is-invalid' : '' }}" name="n8" placeholder="8">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 pl-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="9">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n9') ? ' is-invalid' : '' }}" name="n9" placeholder="9">
                 </div>
             </div>
             <div class="form-group mb-2 row">
                 <div class="col-4 col-md-2 col-lg-2 pr-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="10">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n10') ? ' is-invalid' : '' }}" name="n10" placeholder="10">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 p-0">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="11">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n11') ? ' is-invalid' : '' }}" name="n11" placeholder="11">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 pl-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="12">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n12') ? ' is-invalid' : '' }}" name="n12" placeholder="12">
                 </div>
             </div>
             <div class="form-group mb-1 row">
                 <div class="col-4 col-md-2 col-lg-2 pr-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="13">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n13') ? ' is-invalid' : '' }}" name="n13" placeholder="13">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 p-0">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="14">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n14') ? ' is-invalid' : '' }}" name="n14" placeholder="14">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 pl-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="15">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n15') ? ' is-invalid' : '' }}" name="n15" placeholder="15">
                 </div>
             </div>
             <div class="form-group mb-1 row">
                 <div class="col-4 col-md-2 col-lg-2 pr-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="16">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n16') ? ' is-invalid' : '' }}" name="n16" placeholder="16">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 p-0">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="17">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n17') ? ' is-invalid' : '' }}" name="n17" placeholder="17">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 pl-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="18">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n18') ? ' is-invalid' : '' }}" name="n18" placeholder="18">
                 </div>
             </div>
             <div class="form-group mb-1 row">
                 <div class="col-4 col-md-2 col-lg-2 pr-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="19">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n19') ? ' is-invalid' : '' }}" name="n19" placeholder="19">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 p-0">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="20">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n20') ? ' is-invalid' : '' }}" name="n20" placeholder="20">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 pl-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="21">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n21') ? ' is-invalid' : '' }}" name="n21" placeholder="21">
                 </div>
             </div>
             <div class="form-group mb-2 row">
                 <div class="col-4 col-md-2 col-lg-2 pr-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="22">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n22') ? ' is-invalid' : '' }}" name="n22" placeholder="22">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 p-0">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="23">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n23') ? ' is-invalid' : '' }}" name="n23" placeholder="23">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 pl-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="24">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n24') ? ' is-invalid' : '' }}" name="n24" placeholder="24">
                 </div>
             </div>
             <div class="form-group mb-1 row">
                 <div class="col-4 col-md-2 col-lg-2 pr-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="25">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n25') ? ' is-invalid' : '' }}" name="n25" placeholder="25">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 p-0">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="26">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n26') ? ' is-invalid' : '' }}" name="n26" placeholder="26">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 pl-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="27">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n27') ? ' is-invalid' : '' }}" name="n27" placeholder="27">
                 </div>
             </div>
             <div class="form-group mb-1 row">
                 <div class="col-4 col-md-2 col-lg-2 pr-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="28">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n28') ? ' is-invalid' : '' }}" name="n28" placeholder="28">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 p-0">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="29">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n29') ? ' is-invalid' : '' }}" name="n29" placeholder="29">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 pl-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="30">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n30') ? ' is-invalid' : '' }}" name="n30" placeholder="30">
                 </div>
             </div>
             <div class="form-group mb-1 row">
                 <div class="col-4 col-md-2 col-lg-2 pr-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="31">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n31') ? ' is-invalid' : '' }}" name="n31" placeholder="31">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 p-0">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="32">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n32') ? ' is-invalid' : '' }}" name="n32" placeholder="32">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 pl-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="33">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n33') ? ' is-invalid' : '' }}" name="n33" placeholder="33">
                 </div>
             </div>
             <div class="form-group mb-2 row">
                 <div class="col-4 col-md-2 col-lg-2 pr-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="34">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n34') ? ' is-invalid' : '' }}" name="n34" placeholder="34">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 p-0">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="35">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n35') ? ' is-invalid' : '' }}" name="n35" placeholder="35">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 pl-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="36">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n36') ? ' is-invalid' : '' }}" name="n36" placeholder="36">
                 </div>
             </div>
             <div class="form-group mb-2 row">
                 <div class="col-4 col-md-2 col-lg-2 pr-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="2 to 1">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n2to11') ? ' is-invalid' : '' }}" name="n2to11" placeholder="2 to 1">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 p-0">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="2 to 1">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n2to12') ? ' is-invalid' : '' }}" name="n2to12" placeholder="2 to 1">
                 </div>
                 <div class="col-4 col-md-2 col-lg-2 pl-1">
-                    <input type="number" class="form-control form-control-sm text-center" placeholder="2 to 1">
+                    <input type="number" class="form-control form-control-sm text-center{{ $errors->has('n2to13') ? ' is-invalid' : '' }}" name="n2to13" placeholder="2 to 1">
                 </div>
             </div>
         </div>
