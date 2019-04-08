82c3e8da987deea29cdf5e44556ae88a9719723a
Alex <alexzoetermeer@gmail.com>
2019-04-08 18:45:41 +0200

    Fixed a bank bug for inflated amounts

--- app/Helpers/InflateAmount.php
+++ app/Helpers/InflateAmount.php
@@ -16,41 +16,41 @@ if (!function_exists('inflateAmount')) {
      */
     function inflateAmount(int $amount)
     {
-        if ($amount < 50000)
+        if ($amount < 50001)
         {
             return floor($amount * 1.12);
         }
 
-        if ($amount > 50000 && $amount < 100000)
+        if ($amount > 50000 && $amount < 100001)
         {
             return floor($amount * 1.10);
         }
 
-        if ($amount > 100001 && $amount < 250000)
+        if ($amount > 100000 && $amount < 250001)
         {
             return floor($amount * 1.09);
         }
 
-        if ($amount > 250001 && $amount < 500000)
+        if ($amount > 250000 && $amount < 500001)
         {
             return floor($amount * 1.08);
         }
 
-        if ($amount > 500001 && $amount < 1000000)
+        if ($amount > 500000 && $amount < 1000001)
         {
             return floor($amount * 1.07);
         }
 
-        if ($amount > 1000001 && $amount < 10000000)
+        if ($amount > 1000000 && $amount < 10000001)
         {
             return floor($amount * 1.06);
         }
 
-        if ($amount > 10000001)
+        if ($amount > 10000000)
         {
-            return floor($amount * 1.02); 
+            return floor($amount * 1.02);
         }
 
-        return 'Low-life';
+        return floor($amount * 1.02); 
     }
 }
