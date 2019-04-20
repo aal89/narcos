b59ece8a7fe9cbc3ab206fc391b6aecf25af8112
Alex <alexzoetermeer@gmail.com>
2019-04-20 14:22:13 +0200

    Added stats table to frontpage

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,9 +33,96 @@
                 </div>
             </div>
             <h4 class="mt-4">Statistics</h4>
-            <p>
-            {{ App\Stats::totalCrimeAttempts() }}
-            </p>
+            <table class="table table-sm table-dark">
+                <thead>
+                    <tr>
+                        <th colspan="2">{{ __('Totals and such...') }}</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td class="cell-fit">
+                            Money in-game
+                        </td>
+                        <td>
+                            &euro;{{ number_format(App\Stats::totalMoney(), 0, '.', '.') }},-
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="cell-fit">
+                            Bullets in-game
+                        </td>
+                        <td>
+                            {{ number_format(App\Stats::totalBullets(), 0, '.', '.') }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="cell-fit">
+                            Last registered character
+                        </td>
+                        <td>
+                            {{ App\Stats::newestCharacter()->name }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="cell-fit">
+                            Last registered character
+                        </td>
+                        <td>
+                            {{ App\Stats::highestRankingCharacter()->name }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="cell-fit">
+                            Crime attempts
+                        </td>
+                        <td>
+                            {{ number_format(App\Stats::totalCrimeAttempts(), 0, '.', '.') }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="cell-fit">
+                            Organized crime attempts
+                        </td>
+                        <td>
+                            {{ number_format(App\Stats::totalOrganizedCrimeAttempts(), 0, '.', '.') }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="cell-fit">
+                            Kill attempts (that failed)
+                        </td>
+                        <td>
+                            {{ number_format(App\Stats::totalKillFail(), 0, '.', '.') }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="cell-fit">
+                            Kill attempts (that succeeded)
+                        </td>
+                        <td>
+                            {{ number_format(App\Stats::totalKillSuccess(), 0, '.', '.') }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="cell-fit">
+                            Money lost at the numbers game
+                        </td>
+                        <td>
+                            &euro;{{ number_format(App\Stats::totalNumbersGameLoss(), 0, '.', '.') }},-
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="cell-fit">
+                            Money lost at the roulette
+                        </td>
+                        <td>
+                            &euro;{{ number_format(App\Stats::totalRouletteLoss(), 0, '.', '.') }},-
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
         <div class="col-md-4 col-lg-4">
             <form method="POST" action="{{ route('login') }}">
