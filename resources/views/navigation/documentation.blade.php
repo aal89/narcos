234d112dc8f85a59cea5f15925e3af0e8f62a401
Alex <alexzoetermeer@gmail.com>
2019-04-20 20:15:53 +0200

    Added killing documentation

--- resources/views/navigation/documentation.blade.php
+++ resources/views/navigation/documentation.blade.php
@@ -13,6 +13,14 @@
                 <p>All calculations, additions and substractions regarding your character and the game are described in more detail on this page. However you cannot derive any rights from this page. They are merely meant as informative. You could use it to determine a strategy.</p>
             </div>
             <div class="row">
+                <h5 class="text-secondary" id="profile">Character</h5>
+            </div>
+            <div class="row">
+                <p>
+                    Char.
+                </p>
+            </div>
+            <div class="row">
                 <h5 class="text-secondary" id="profile">Banking</h5>
             </div>
             <div class="row">
@@ -120,20 +128,90 @@
                         </tr>
                         <tr>
                             <td>:br:</td>
-                            <td><br><i>*An empty line*</i></td>
+                            <td><i>*An empty line*</i></td>
                         </tr>
                         <tr>
                             <td>:link:<font color=red>https://www.google.com</font>:-link:<font color=red>Google</font>:/link:</td>
-                            <td><a href="https://www.google.com">Google</a></td>
+                            <td><a class="not-bold" href="https://www.google.com">Google</a></td>
                         </tr>
                     </tbody>
                 </table>
             </div>
             <div class="row">
-                <h5 class="text-secondary">etc</h5>
+                <h5 class="text-secondary" id="profile">Crime</h5>
             </div>
             <div class="row">
-                <p>TODO</p>
+                <p>
+                    Both trivial crimes and organized crimes are the only ways for you to rank up your character. Buying/selling dope, killing someone,
+                    buying property, banking, traveling or gambling are not going to level up your character. Throughout your career as a drugdealer
+                    these types or crimes are increasingly important. Never give up leveling up your character. We use the experience count in the game
+                    for various aspects.
+                </p>
+            </div>
+            <div class="row">
+                <h5 class="text-secondary" id="profile">Killing</h5>
+            </div>
+            <div class="row">
+                <p>
+                    Killing is a serious business, so the technical implementation is serious too. Once a character is dead it is no longer
+                    playable.
+                </p>
+                <p>
+                    In the code we keep track of a characters experience count. We use it to display someones rank and to determine how 'strong'
+                    this character is. To determine how many bullets you need to kill someone we use this formula:
+                    <code>8998 + (196 - 8998) / (1 + ($exp / 707677) ^ 0.5319177)</code>.
+                    The <code>$exp</code> is replaced with the targets experience count. This results in a graph like this:
+                </p>
+                <p>
+                    <img class="img-fluid mb-2" src="/bullets-curve.png" />
+                    <i>Note: each red dot is rank, 1 million exp (x-axis) is required to get to rank <b>Kingpin</b></i>. The y-axis is the
+                    number of bullets required.
+                </p>
+                <p>
+                    This graph has a certain number of properties. Firstly you build up defenses really quick, and the old veterans who have been
+                    playing for years are not immortal.
+                </p>
+                <p>
+                    The number from the equation is not final. More factors come into play. Each bullet fired at a target has up to 10% chance
+                    to shatter or not fire at all. Then the weapon used to fire these bullets introduces another factor. The only weapon to maximise
+                    bullet damage is the <b>M-16</b>, it's effectiveness is about 100%. The graph drawn above is plotted with the <b>M-16</b> in mind.
+                    Bullet damage is implicit and this number is different for different weapons.
+                </p>
+                <table class="table table-sm table-dark">
+                    <thead>
+                        <tr>
+                            <th colspan="2">{{ __('Weapon effectiveness') }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Glock</td>
+                            <td>70% - 80%</td>
+                        </tr>
+                        <tr>
+                            <td>Shotgun</td>
+                            <td>
+                                80% - 90%
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Ak-47</td>
+                            <td>90% - 100%</td>
+                        </tr>
+                        <tr>
+                            <td>M-16</td>
+                            <td>100%</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="row">
+                <h5 class="text-secondary" id="profile">Map</h5>
+            </div>
+            <div class="row">
+                <p>
+                    Char.
+                </p>
             </div>
         </div>
     </div>
