cf5336e3f1290a567d14a54d949576105fd90f81
Alex <alexzoetermeer@gmail.com>
2019-04-22 14:17:48 +0200

    Updated news item

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,8 @@
     <div class="row">
         - Made the login/homepage more appealing.<br>
         - Added statistics page.<br>
+        - Organized crime now gives more money and experiensce.<br>
+        - Crime experience gains are now proportional to the succession rate.<br>
     </div>
     <div class="row">
         <span class="small mt-2 mb-2">20-04-2019 14:45 by aal</span>
