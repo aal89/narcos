4af4eda9236e2332cfec817dc0188fc8a5baf460
Alex <alexzoetermeer@gmail.com>
2019-04-24 21:26:20 +0200

    Added migration and middleware logic to keep track of last used ip

--- app/Http/Middleware/LogUserActivity.php
+++ app/Http/Middleware/LogUserActivity.php
@@ -19,6 +19,14 @@ class LogUserActivity
     public function handle($request, Closure $next)
     {
         if(Auth::check()) {
+            // save last used ip if it differs
+            $user = Auth::user();
+
+            if($user->last_used_ip !== $request->ip()) {
+                $user->last_used_ip = $request->ip();
+                $user->save();
+            }
+
             $expiresAt = Carbon::now()->addMinutes(5);
             // todo: when application grows, use different cache provider (fs is not ideal)
             Cache::put('user-is-online-' . Auth::user()->character->id, true, $expiresAt);
